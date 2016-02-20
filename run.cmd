779d6261db38e91b32e4a1533a4b2622224c112f
hulr <roland.huegli@outlook.com>
2016-02-20 19:26:11 +0100

    Additional OS verification

--- run.cmd
+++ run.cmd
@@ -1,2 +1,11 @@
 @echo off
+ver | find "Version 10." > nul
+if %ERRORLEVEL% == 0 (
 powershell.exe -executionpolicy bypass -file BetterPrivacy.ps1
+) else (
+echo your operating system is not supported
+echo please take a look at the system requirements stated in the README.md file
+echo.
+pause
+exit
+)
